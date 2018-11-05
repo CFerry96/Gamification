16ff3b1d5220909dd8fc212a4448da4efc26d727
CFerry <c16349346@mydit.ie>
2018-11-05 14:14:58 +0000

    Fixed Scriot Error

---
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/Scripts/Cillian Scripts/SelectKingdom.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectKingdom : MonoBehaviour {
+    public string Kingdom;
+
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene(Kingdom);
+    }
+}
