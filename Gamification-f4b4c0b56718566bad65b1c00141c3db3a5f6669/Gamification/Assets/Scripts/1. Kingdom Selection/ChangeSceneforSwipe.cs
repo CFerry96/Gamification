7650c827f0d56ce9e972a8755f339fdef2a9a6ae
Patnjop <36369847+Patnjop@users.noreply.github.com>
2018-11-18 17:25:19 +0000

    Spotlights display active kingdom

--- Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/Scripts/1. Kingdom Selection/ChangeSceneforSwipe.cs
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/Scripts/1. Kingdom Selection/ChangeSceneforSwipe.cs
@@ -7,16 +7,30 @@ public class ChangeSceneforSwipe : MonoBehaviour {
     public string ScenetoLoad;
     touchSwipe swipe;
     GameObject Book;
+    public string KingdomName;
+    public GameObject Spotlight;
+    public GameObject[] Spotlights;
 
 	// Use this for initialization
 	void Start () {
         Book = GameObject.Find("New Book");
+        Spotlight = GameObject.Find(KingdomName + " Spot Light");
+        Spotlight.SetActive(false);
         swipe = Book.GetComponent<touchSwipe>();
 	}
 
     // Update is called once per frame
     private void OnMouseDown()
     {
+        for (int i = 0; i < Spotlights.Length; i++)
+        {
+            Spotlights[i].SetActive(false);
+        }
+        Spotlight.SetActive(true);
         swipe.PageNext = ScenetoLoad;
+    }
+    private void Update()
+    {
+        Spotlights = GameObject.FindGameObjectsWithTag("Spotlight");
     }
 }
