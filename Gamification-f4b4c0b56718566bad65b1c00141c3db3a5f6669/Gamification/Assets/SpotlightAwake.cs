337990aca8902acf6bab1d7417817ada6e9a6972
Patnjop <36369847+Patnjop@users.noreply.github.com>
2018-11-18 18:19:57 +0000

    Visual touches added to spotlights

---
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/SpotlightAwake.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightAwake : MonoBehaviour {
+
+    
+    private void OnEnable()
+    {
+        this.GetComponent<Light>().intensity = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (this.GetComponent<Light>().intensity <= 23)
+           {
+            this.GetComponent<Light>().intensity++;
+           }
+	}
+}
