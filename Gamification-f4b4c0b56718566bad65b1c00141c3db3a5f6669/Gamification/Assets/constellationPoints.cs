a6716f7cf5edddb8a8565c4cf53b829befd17ba9
Mark <c16765279@mydit.ie>
2018-11-21 20:29:10 +0000

    added stars

---
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/constellationPoints.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class constellationPoints : MonoBehaviour {
+    public GameObject starPrefab;
+    public GameObject[] starryBoys = new GameObject[6];
+
+	// Use this for initialization
+	void Start () {
+        for(int i=0; i< 6; i++)
+        {
+            starryBoys[i] = (GameObject)Instantiate(starPrefab, new Vector3( (transform.position.x + persistence.control.positionArray[i].x), 
+                (transform.position.y + persistence.control.positionArray[i].y), 
+                (transform.position.z +persistence.control.positionArray[i].z)), 
+                Quaternion.identity);
+            starryBoys[i].transform.parent = gameObject.transform;
+            
+        }
+        
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
