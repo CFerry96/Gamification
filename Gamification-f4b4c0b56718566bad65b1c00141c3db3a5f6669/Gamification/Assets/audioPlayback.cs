961c34dfb6f40b33280d0d097bc67a71d0c0d3d5
Mark <c16765279@mydit.ie>
2018-12-08 14:27:41 +0000

    added audio play back to scene 1

---
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/audioPlayback.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class audioPlayback : MonoBehaviour {
+    AudioSource audio;
+    public Image playButton;
+    public GameObject buttonToDisplay;
+
+    // Use this for initialization
+    void Start () {
+        audio = GetComponent<AudioSource>();
+        StartCoroutine(CheckForButton());
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (audio.isPlaying)
+        {
+            playButton.color = Color.green;
+        }
+        else
+        {
+            playButton.color = Color.white;
+        }
+        
+    }
+
+    public void PlayAudio()
+    {
+        //AudioSource audio = GetComponent<AudioSource>(); //
+        //audio.clip = myAudioClip;
+        //audio.Play();
+        AudioSerialisation.LoadAudioClipFromDisk(audio, "myfile");
+        audio.Play();
+    }
+
+    IEnumerator CheckForButton()
+    {
+        while (true)
+        {
+            if (persistence.control.activated)
+            {
+                buttonToDisplay.SetActive(true);
+                Debug.Log("button on");
+            }
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
