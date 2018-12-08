f0ca3fc0f5ee28e77a6da1c2dacd08006669a918
Mark <c16765279@mydit.ie>
2018-12-08 14:01:22 +0000

    fixed dragging stars bug

---
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/dragToStars.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragToStars : MonoBehaviour {
+    Camera cam;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetMouseButton(0))
+        {
+
+            Ray ray = cam.ViewportPointToRay(cam.ScreenToViewportPoint(Input.mousePosition));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 1000))
+                if (hit.transform.tag == "Interactable")
+                {
+                    //print("I'm looking at " + hit.transform.name);
+                    hit.transform.gameObject.SendMessage("clicked");
+                    //Destroy(hit.transform.gameObject);
+                }
+                else
+                {
+                    //print("I'm looking at nothing!");
+                }
+        }
+    }
+}
