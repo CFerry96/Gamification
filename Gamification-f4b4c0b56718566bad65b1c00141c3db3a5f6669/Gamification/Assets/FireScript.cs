38fb9694c1c3bee7908356eaaaa0a187eaf22f42
CFerry <c16349346@mydit.ie>
2018-12-10 14:59:02 +0000

    Fire Column Added

---
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/FireScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireScript : MonoBehaviour {
+
+    private bool lit = false;
+    public GameObject Fire;
+
+    private void OnMouseDown()
+    {
+        if(lit==false)
+        {
+            lit = true;
+        }
+        else if(lit==true)
+        {
+            lit = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (lit == true)
+        {
+            Fire.SetActive(true);
+        }
+        else if (lit == false)
+        {
+            Fire.SetActive(false);
+        }
+    }
+}
