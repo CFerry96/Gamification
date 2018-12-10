42b4318dcc0f62fc2c1240234a8fe52d4de7f191
Patnjop <36369847+Patnjop@users.noreply.github.com>
2018-12-10 14:20:12 +0000

    Fire imported

--- Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/Scripts/Menu Scripts/BookPauseMenu.cs
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/Scripts/Menu Scripts/BookPauseMenu.cs
@@ -6,17 +6,17 @@ using UnityEngine.UI;
 
 public class BookPauseMenu : MonoBehaviour {
 
-    public bool isPaused;
+    //public bool isPaused;
     public bool settingsOn;
-    GameObject Menu;
+    //GameObject Menu;
     GameObject SettingsMenu;
 
     CanvasAssigner canvasAssigner;
     GameObject Canvas;
 
-    Button continueButton;
+    //Button continueButton;
     Button settingsButton;
-    Button exitButton;
+    //Button exitButton;
     Button exitButton1;
     Button gearButton;
     Button exitFromPage;
@@ -34,12 +34,12 @@ public class BookPauseMenu : MonoBehaviour {
         gearButton = GameObject.Find("Settings Icon").GetComponent<Button>();
         exitFromPage = GameObject.Find("Exit Icon").GetComponent<Button>();
 
-        continueButton = canvasAssigner.Continue;
+        //continueButton = canvasAssigner.Continue;
         settingsButton = canvasAssigner.Settings;
-        exitButton = canvasAssigner.Exit;
+        //exitButton = canvasAssigner.Exit;
         exitButton1 = canvasAssigner.Exit1;
 
-        Menu = canvasAssigner.PauseMenu;
+        //Menu = canvasAssigner.PauseMenu;
         SettingsMenu = canvasAssigner.SettingsMenu;
 
         
@@ -48,56 +48,54 @@ public class BookPauseMenu : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        continueButton.onClick.AddListener(Continue);
+        //continueButton.onClick.AddListener(Continue);
         settingsButton.onClick.AddListener(Settings);
-        exitButton.onClick.AddListener(Exit);
+        //exitButton.onClick.AddListener(Exit);
         exitButton1.onClick.AddListener(Exit);
         gearButton.onClick.AddListener(Settings);
         exitFromPage.onClick.AddListener(Gtfo);
 
-        if (isPaused == true)
-        {
-            Menu.SetActive(true);
-            Time.timeScale = 0f;
-        }
+        //if (isPaused == true)
+        //{
+        //    Menu.SetActive(true);
+        //    Time.timeScale = 0f;
+        //}
 
-        if (isPaused == false)
-        {
-            Menu.SetActive(false);
-            if (settingsOn == false)
-            {
-                Time.timeScale = 1f;
-            }
-        }
+        //if (isPaused == false)
+        //{
+        //    Menu.SetActive(false);
+        //    if (settingsOn == false)
+        //    {
+        //        Time.timeScale = 1f;
+        //    }
+        //}
 
         if (settingsOn == true)
         {
+            Time.timeScale = 0f;
             SettingsMenu.SetActive(true);
         }
 
         if (settingsOn == false)
         {
+            Time.timeScale = 1f;
             SettingsMenu.SetActive(false);
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            isPaused = !isPaused;
-            if (settingsOn == true)
-            {
-                settingsOn = false;
-            }
+            settingsOn = !settingsOn;
         }
     }
     public void Continue()
     {
-        isPaused = false;
+        //isPaused = false;
         settingsOn = false;
     }
 
     public void Settings()
     {
-        isPaused = false;
+        //isPaused = false;
         settingsOn = true;
     }
 
@@ -107,10 +105,10 @@ public class BookPauseMenu : MonoBehaviour {
         {
             settingsOn = false;
         }
-        if (isPaused == true)
-        {
-            isPaused = false;
-        }
+        //if (isPaused == true)
+        //{
+        //    isPaused = false;
+        //}
     }
 
     public void Gtfo()
