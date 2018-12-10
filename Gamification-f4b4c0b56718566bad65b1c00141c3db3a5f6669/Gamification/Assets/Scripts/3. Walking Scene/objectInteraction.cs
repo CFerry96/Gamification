0712e9b5c5cd399f449ff10d7b8e2549492342b1
Mark <c16765279@mydit.ie>
2018-12-10 15:16:04 +0000

    Revert "changed button to object in scene 1"
    This reverts commit 9151c225c3ba43554c1852195192f629e13f96f3.

--- Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/Scripts/3. Walking Scene/objectInteraction.cs
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/Scripts/3. Walking Scene/objectInteraction.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class objectInteraction : MonoBehaviour {
-    Camera cam;
+    public Camera cam;
     
 
     // Use this for initialization
