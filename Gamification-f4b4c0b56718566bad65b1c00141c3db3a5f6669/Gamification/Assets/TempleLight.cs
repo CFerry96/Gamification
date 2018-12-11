d5a8970098893b83c315626d247d4b060225fd78
CFerry96 <c16349346@mydit.ie>
2018-12-11 15:58:03 +0000

    Temple Light trigger

---
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/TempleLight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempleLight : MonoBehaviour {
+    private bool lit = false;
+    private Light solas;
+
+	// Use this for initialization
+	void Start () {
+        solas = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(lit == true)
+        {
+            solas.enabled = true;
+        }
+        else if(lit == false)
+        {
+            solas.enabled = false;
+        }
+	}
+
+    private void OnMouseDown()
+    {
+        if(lit == false)
+        {
+            lit = true;
+        }
+        else if(lit == true)
+        {
+            lit = false;
+        }
+    }
+}
