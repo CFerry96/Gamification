5fd12eed00fa7b633fe59b59b226e3e2181947a9
CFerry96 <c16349346@mydit.ie>
2018-12-12 02:53:24 +0000

    Create Button Code

---
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/Create.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Create : MonoBehaviour {
+
+    public Camera cam;
+    public GameObject book;
+
+	void Update () {
+        
+	}
+
+    private void OnMouseDown()
+    {
+        cam.transform.rotation = Quaternion.Slerp(transform.position,book.transform.position, Time.deltaTime * 5);
+    }
+            
+    }
