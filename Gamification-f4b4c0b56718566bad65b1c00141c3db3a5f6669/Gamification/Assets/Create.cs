c708bab7740a3f2da3ffcf00e1aa1a056d6aae4f
CFerry96 <c16349346@mydit.ie>
2018-12-12 03:07:56 +0000

    Main Menu Slerp

--- Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/Create.cs
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/Create.cs
@@ -6,14 +6,18 @@ public class Create : MonoBehaviour {
 
     public Camera cam;
     public GameObject book;
+    private bool clicked = false;
 
 	void Update () {
-        
+        if(clicked == true)
+        {
+            cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation, book.transform.rotation, Time.deltaTime);
+        }
 	}
 
     private void OnMouseDown()
     {
-        cam.transform.rotation = Quaternion.Slerp(this.transform.rotation,book.transform.rotation, Time.deltaTime);
+        clicked = true;
     }
             
     }
