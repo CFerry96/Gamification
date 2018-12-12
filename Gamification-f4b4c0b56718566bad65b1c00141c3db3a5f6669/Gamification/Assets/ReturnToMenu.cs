f9acb6f5b8e94b5fa6835e2431f551eec3a753bf
CFerry <c16349346@mydit.ie>
2018-12-12 11:57:33 +0000

    Return To Book Button Added To Medititaion Scene

---
+++ Gamification-f4b4c0b56718566bad65b1c00141c3db3a5f6669/Gamification/Assets/ReturnToMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ReturnToMenu : MonoBehaviour {
+
+    public Text button;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (button)
+            {
+                SceneManager.LoadScene("Page 1");
+            }
+        }
+
+    }
+    
+}
